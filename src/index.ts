0ef520f9190ce2b0db73daac8ec41e7189d94898
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-01 15:07:03 -0200

    Defining basic API

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import {UserProfile} from "./profile";
+import {Auth0Properties} from "./properties";
+
+export {configure, isAuthenticated, signIn, signOut, getProfile};
+
+// the following functions are exported
+
+function configure(properties: Auth0Properties): void {
+
+}
+
+function isAuthenticated(): boolean {
+    return false;
+}
+
+function signIn(): void {
+
+}
+
+function signOut(): void {
+
+}
+
+function getProfile(): UserProfile | null {
+    return null;
+}
+
+// the following functions are private
+
+function loadProfile(): void {
+
+}
+
+function setSignedIn(signedIn: boolean): void {
+
+}
+
+function parsesHash(): void {
+
+}
---
+++ src/properties.ts
@@ -0,0 +1,15 @@
+export {Auth0Properties};
+
+// Auth0Properties was based on https://github.com/auth0/auth0.js
+
+// as we favor convention over configuration and to keep things easy,
+// we removed responseMode and responseType from this version
+
+class Auth0Properties {
+    constructor(public domain: string,
+                public clientID: string,
+                public redirectUri?: string,
+                public scope?: string,
+                public audience?: string,
+                public disableWarnings = false)
+}
