278e2d016f032b1d2016d2360bd8dfea850e63bc
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-20 17:18:11 -0200

    would it be last bug?

--- dist/index.js
+++ dist/index.js
@@ -89,7 +89,7 @@ function silentAuth(tokenName, audience, scopes) {
         }
         var extraTokens = JSON.parse(localStorage.getItem(EXTRA_TOKENS));
         extraTokens[tokenName] = authResult.accessToken;
-        localStorage.setItem(EXTRA_TOKENS, extraTokens);
+        localStorage.setItem(EXTRA_TOKENS, JSON.stringify(extraTokens));
     });
 }
 exports.silentAuth = silentAuth;
